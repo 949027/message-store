aa67341fa1664e7fd78f6369d2bb7c50feaa17a7
949027 <82878668+949027@users.noreply.github.com>
2022-09-04 14:28:00 +0700

    Tests added

--- message_store_app/tests.py
+++ message_store_app/tests.py
@@ -1,27 +1,43 @@
 import requests
+from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APIClient
 
 
-class ModelsTestCase(TestCase):
-    pass
+client = APIClient()
 
 
-# class ViewsTestCase(TestCase):
-#     def test_index_loads_properly(self):
-#         username = 'test_user'
-#         password = 'Qq123456'
-#
-#         user = User.objects.create(username=username, password=password)
-#
-#         url = f'http://127.0.0.1:8000/app/token/'
-#         json = {
-#             'username': 'qwerty123',
-#             'password': 'Qq367077',
-#         }
-#         response = requests.post(url, json=json)
-#
-#         response.raise_for_status()
-#
-#         response = self.client.post('http://127.0.0.1:8000/app/message/', headers=headers, json=json)
-#         self.assertEqual(response.status_code, 200)
+class ViewsTestCase(TestCase):
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(username='test_user',
+                                                         password='test1234',
+                                                         email='test@example.com')
+
+    def test_sending_token(self):
+        data = {'username': 'test_user', 'password': 'test1234'}
+        response = client.post(reverse('obtain_auth_token'), data, format='json')
+        token = response.data['token']
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(token), 40)
+
+    def test_receiving_message(self):
+        data = {'message': 'hello'}
+        client.force_authenticate(user=self.user)
+        response = client.post(reverse('process_request'), data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['message'], 'Message saved')
+
+    def test_sending_history(self):
+        client.force_authenticate(user=self.user)
+        for _ in range(10):
+            data = {'message': 'hello'}
+            client.post(reverse('process_request'), data, format='json')
+        data = {'message': 'history 5'}
+        response = client.post(reverse('process_request'), data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 5)
